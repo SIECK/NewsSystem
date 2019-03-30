66378cfac1341954337126c6c4f806bec0049728
Krzysztof Gonicki <210189@edu.p.lodz.pl>
2019-03-30 18:46:58 +0100

    Dokończenie pracy nad testami. Dodanie kompresji gzip.

--- src/main/java/com/newssystem/NewsSystem/rest/AppRESTController.java
+++ src/main/java/com/newssystem/NewsSystem/rest/AppRESTController.java
@@ -5,8 +5,11 @@ import com.newssystem.NewsSystem.domain.News;
 import com.newssystem.NewsSystem.service.CommentService;
 import com.newssystem.NewsSystem.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,8 +17,10 @@ import java.util.Map;
 @RestController
 @RequestMapping("api/news")
 public class AppRESTController {
+
     private final CommentService commentService;
     private final NewsService newsService;
+    private final Map<String, Object> response = new LinkedHashMap<>();
 
     @Autowired
     public AppRESTController(CommentService commentService, NewsService newsService) {
@@ -34,20 +39,34 @@ public class AppRESTController {
     List<Comment> findAllComments() {return commentService.getObj(); }
 
     @RequestMapping(method = RequestMethod.POST, value = "/saveNews")
-    public @ResponseBody Map<String, Object> create(@RequestBody News newsEntity){
-
-        Map<String, Object> response = new LinkedHashMap<>();
-        newsService.create(newsEntity);
-        response.put("message", "News created");
-        response.put("news", newsService.create(newsEntity));
-
+    public @ResponseBody Map<String, Object> create(@Valid @RequestBody News newsEntity, BindingResult bindingResult){
+        if(checkError(bindingResult)){
+            newsService.create(newsEntity);
+            response.put("message", "News created successfully");
+        }
         return response;
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/saveComment")
-    public @ResponseBody
-    Comment create(@RequestBody Comment commentEntity){
-        return commentService.create(commentEntity);
+    public @ResponseBody Map<String, Object> create(@Valid @RequestBody Comment commentEntity, BindingResult bindingResult){
+        if(checkError(bindingResult)){
+            commentService.create(commentEntity);
+            response.put("message", "Comment created successfully");
+        }
+        return response;
     }
 
+    public boolean checkError(BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            response.put("message", "Error");
+            for (FieldError error: errors){
+                response.put(error.getField(), error.getDefaultMessage());
+            }
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 }
