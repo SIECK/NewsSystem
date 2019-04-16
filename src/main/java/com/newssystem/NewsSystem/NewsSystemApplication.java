51f7a5db67aeca7e705056b39ff70e35230ccc3b
Krzysztof Gonicki <210189@edu.p.lodz.pl>
2019-04-16 13:13:08 +0200

    gitignore update

--- src/main/java/com/newssystem/NewsSystem/NewsSystemApplication.java
+++ src/main/java/com/newssystem/NewsSystem/NewsSystemApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
